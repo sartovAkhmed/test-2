ae8f07c51217a2723b6a1b9cd1d67b6602cb1dfe
Akhmed <akhmedturan@gmail.com>
2023-12-01 23:12:04 +0600

    test-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, useCallback } from 'react';
 
-function App() {
+const TaskList = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, name: 'Task 1', status: 'pending' },
+    { id: 2, name: 'Task 2', status: 'completed' },
+    { id: 3, name: 'Task 3', status: 'pending' },
+    { id: 4, name: 'Task 4', status: 'in-progress' },
+  ]);
+
+  const [filter, setFilter] = useState('all');
+
+  const filteredTasks = useMemo(() => {
+    console.log('Filtering tasks...');
+    if (filter === 'all') {
+      return tasks;
+    } else {
+      return tasks.filter(task => task.status === filter);
+    }
+  }, [tasks, filter]);
+
+  const handleFilterChange = useCallback(
+    selectedFilter => {
+      setFilter(selectedFilter);
+    }, []
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label>
+          Filter:
+          <select value={filter} onChange={(e) => handleFilterChange(e.target.value)}>
+            <option value="all">All</option>
+            <option value="pending">Pending</option>
+            <option value="completed">Completed</option>
+            <option value="in-progress">In Progress</option>
+          </select>
+        </label>
+      </div>
+
+      <ul>
+        {filteredTasks.map(task => (
+          <li key={task.id}>
+            {task.name} - {task.status}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default TaskList;
